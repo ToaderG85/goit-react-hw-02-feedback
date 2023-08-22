f4f13f7af1b2cb7fae7ab72dbedf5ba8233a000d
ToaderG85 <123585550+ToaderG85@users.noreply.github.com>
2023-08-22 22:10:18 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
+import React, { useState } from "react";
+import Buttons from "./Buttons/Buttons";
+import Statistics from "./Statistics/Statistics";
+import Notification from "./Notification/Notification";
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+
+  const [data, setData] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const feedbackOptions = Object.keys(data);
+
+  const handleFeedback = (currentFeedback) => {
+    setData((prev) => ({
+      ...prev, [currentFeedback]: prev[currentFeedback] + 1, 
+    }));
+  };
+
+  const totalFeedbackHitsCount = () => {
+    return Object.values(data).reduce((total, element) => total + element, 0);
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = data;
+    return Math.round((good / totalFeedbackHitsCount()) *100);
+  };
+
+  const totalFeedbackHits = totalFeedbackHitsCount();
+  const positiveFeedback = countPositiveFeedbackPercentage();
+
+   return (
+    <div>      
+      <Buttons
+        feedbackOptions={feedbackOptions}
+        onFeedback={handleFeedback}
+      />
+      {totalFeedbackHits ? (<Statistics
+          good={data.good}
+          neutral={data.neutral}
+          bad={data.bad}
+          totalFeedback={totalFeedbackHits}
+          positiveFeedback={positiveFeedback}
+      />) : ( <Notification message={'There is no feedback'} />)}
     </div>
   );
 };
